16af1e3fe02f8aba6eceb5a6ddf1662c52c46a48
adviservs <adviservs@gmail.com>
2024-01-18 16:47:55 +1000

    Variables

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$name = 'Viktor';
+$age = 20;
+$hobby = 'swimming';
+$isMarried = true;
+$pet = 'cat';
+$petName = 'Barsik';
+
+echo $name . "\n";
+echo $age . "\n";
+echo $hobby . "\n";
+echo $isMarried . "\n";
+echo $pet . "\n";
+echo $petName . "\n";
