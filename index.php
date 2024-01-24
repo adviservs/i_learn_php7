58ce1deea9e8fbb9ab8040d438697af6c78d9de7
adviservs <adviservs@gmail.com>
2024-01-24 16:44:56 +1000

    arrays add, edit and delete an item to the array homework

--- index.php
+++ index.php
@@ -1,4 +1,32 @@
 <?php
+
+$person = [
+    'name' => 'Viktor',
+    'age' => 20,
+    'hobby' => 'swimming',
+    'isMarried' => true,
+    'pet' => 'cat',
+    'pet_name' => 'Barsik'
+];
+
+$person2 = [
+    'name' => 'Roma',
+    'age' => 42,
+    'hobby' => 'auto',
+    'isMarried' => false,
+    'pet' => 'dog',
+    'pet_name' => 'Jesy'
+];
+
+$person3 = [
+    'name' => 'Igor',
+    'age' => 30,
+    'hobby' => 'gym',
+    'isMarried' => true,
+    'pet' => 'no',
+    'pet_name' => 'No name'
+];
+
 $name = 'Viktor';
 $age = 20;
 $hobby = 'swimming';
@@ -6,9 +34,14 @@ $isMarried = true;
 $pet = 'cat';
 $petName = 'Barsik';
 
-echo $name . "\n";
-echo $age . "\n";
-echo $hobby . "\n";
-echo $isMarried . "\n";
-echo $pet . "\n";
-echo $petName . "\n";
+$person['car'] = 'Toyota'; // add an item to the array
+$person['hobby'] = 'gym'; // edit an item to the array
+unset($person['car']); // delete an item to the array
+print_r($person);
+
+//echo $name . "\n";
+//echo $age . "\n";
+//echo $hobby . "\n";
+//echo $isMarried . "\n";
+//echo $pet . "\n";
+//echo $petName . "\n";
