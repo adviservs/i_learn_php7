c00860ac8758786afd5434fce8ca49bfe601dcd4
adviservs <adviservs@gmail.com>
2024-01-24 17:30:14 +1000

    PHP course for Laravel. 6 Multidimensional arrays

--- index.php
+++ index.php
@@ -1,57 +1,58 @@
 <?php
 
-$person = [
-    'name' => 'Viktor',
-    'age' => 20,
-    'hobby' => 'swimming',
-    'isMarried' => true,
-    'pet' => 'cat',
-    'pet_name' => 'Barsik'
+$persons = [
+    [
+        'name' => 'Viktor',
+        'age' => 20,
+        'hobby' => 'swimming',
+        'isMarried' => true,
+        'pet' => 'cat',
+        'pet_name' => 'Barsik',
+        'cars' => ['toyota', 'mazda', 'lexus']
+    ],
+    [
+        'name' => 'Roma',
+        'age' => 42,
+        'hobby' => 'auto',
+        'isMarried' => false,
+        'pet' => 'dog',
+        'pet_name' => 'Jesy'
+    ],
+    [
+        'name' => 'Igor',
+        'age' => 30,
+        'hobby' => 'gym',
+        'isMarried' => true,
+        'pet' => 'no',
+        'pet_name' => 'No name'
+    ]
 ];
 
-$person2 = [
-    'name' => 'Roma',
-    'age' => 42,
-    'hobby' => 'auto',
-    'isMarried' => false,
-    'pet' => 'dog',
-    'pet_name' => 'Jesy'
-];
-
-$person3 = [
-    'name' => 'Igor',
-    'age' => 30,
-    'hobby' => 'gym',
-    'isMarried' => true,
-    'pet' => 'no',
-    'pet_name' => 'No name'
-];
-
-$name = 'Viktor';
-$age = 20;
-$hobby = 'swimming';
-$isMarried = true;
-$pet = 'cat';
-$petName = 'Barsik';
-
-$person['car'] = 'Toyota'; // add an item to the array
-$person['hobby'] = 'gym'; // edit an item to the array
-unset($person['car']); // delete an item to the array
-
-foreach ($person as $key => $item) {
-    echo $key . ': ';
-    echo $item . "\n";
+foreach ($persons as $person) {
+    print_r($person['name'] . "\n");
 }
 
-foreach ($person2 as $key => $item) {
-    echo $key . ': ';
-    echo $item . "\n";
-}
+// add an item to the array
+//$person['car'] = 'Toyota';
+// edit an item to the array
+//$person['hobby'] = 'gym';
+// delete an item to the array
+//unset($person['car']);
 
-foreach ($person3 as $key => $item) {
-    echo $key . ': ';
-    echo $item . "\n";
-}
+//foreach ($person as $key => $item) {
+//    echo $key . ': ';
+//    echo $item . "\n";
+//}
+//
+//foreach ($person2 as $key => $item) {
+//    echo $key . ': ';
+//    echo $item . "\n";
+//}
+//
+//foreach ($person3 as $key => $item) {
+//    echo $key . ': ';
+//    echo $item . "\n";
+//}
 
 //print_r($person3);
 
