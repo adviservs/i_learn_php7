88bcc687584772df3f470273e720c1a76c7ae449
adviservs <adviservs@gmail.com>
2024-01-25 15:49:42 +1000

    PHP course for Laravel. 9 Functions

--- index.php
+++ index.php
@@ -1,86 +1,21 @@
 <?php
-
-$persons = [
-    [
-        'name' => 'Viktor',
-        'age' => 20,
-        'hobby' => 'swimming',
-        'isMarried' => false,
-        'pet' => 'cat',
-        'pet_name' => 'Barsik',
-        'cars' => ['toyota', 'mazda', 'lexus']
-    ],
-    [
-        'name' => 'Roma',
-        'age' => 42,
-        'hobby' => 'auto',
-        'isMarried' => false,
-        'pet' => 'dog',
-        'pet_name' => 'Jesy'
-    ],
-    [
-        'name' => 'Igor',
-        'age' => 30,
-        'hobby' => 'gym',
-        'isMarried' => true,
-        'pet' => 'no',
-        'pet_name' => 'No name'
-    ]
-];
-
-//foreach ($persons as $person) {
-//    if ($person['isMarried']) {
-//        print_r($person['name'] . "\n");
-//    } else {
-//        echo 'Not married' . "\n";
-//    }
-//}
-
-$flat = '20';
-$name = 'Mark';
-$age = 20;
-$some = null;
-
-// ==
-// ===
-// &&
-// ||
-// !
-// >
-// <
-// >=
-// >=
-
-
-if ($flat >= 20 && $flat === 20) {
-    echo 'Yes';
-} else {
-    echo 'No';
+// Function Signature - Сигрнатура функции
+function sayHello()
+{
+    echo 'Hello!';
+    echo 'lalala';
+    $name = 'Petr';
+    echo $name;
 }
 
-// add an item to the array
-//$person['car'] = 'Toyota';
-// edit an item to the array
-//$person['hobby'] = 'gym';
-// delete an item to the array
-//unset($person['car']);
+function sum()
+{
+    $sum = 5 + 15;
+    echo $sum;
+}
 
-//foreach ($person as $key => $item) {
-//    echo $key . ': ';
-//    echo $item . "\n";
-//}
-//
-//foreach ($person2 as $key => $item) {
-//    echo $key . ': ';
-//    echo $item . "\n";
-//}
-//
-//foreach ($person3 as $key => $item) {
-//    echo $key . ': ';
-//    echo $item . "\n";
-//}
-
-//print_r($person3);
+sayHello();
+sum();
 
 //echo $name . "\n";
 //echo $age . "\n";
