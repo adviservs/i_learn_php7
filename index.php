0291ff5dcae3d5a754254271b5794a643563d9f1
adviservs <adviservs@gmail.com>
2024-03-07 14:59:09 +1000

    PHP course for Laravel. 12 php classes

--- index.php
+++ index.php
@@ -1,49 +1,13 @@
 <?php
-// Function Signature - Сигрнатура функции
-function sayHello()
-{
-    echo 'Hello!';
-    echo 'lalala';
-    $name = 'Petr';
-    echo $name;
+class Person{
+    public $name = 'Victor';
+    public $age = 20;
+    public $isMarried = true;
+    public function sayHello()
+    {
+        echo 'Hello my friends!';
+    }
 }
 
-//sayHello();
-
-// Sum Сумма
-function getSum($a, $b)
-{
-    $sum = $a + $b;
-    return $sum;
-}
-
-// subtraction function вычитание
-function getSubtraction($a, $b)
-{
-    $sum = $a - $b;
-    return $sum;
-}
-
-// multiplication function умножения
-function getMultiplication($a, $b)
-{
-    $sum = $a * $b;
-    return $sum;
-}
-
-// Fission Деление
-function getFission($a, $b)
-{
-    $sum = $a / $b;
-    return $sum;
-}
-
-$result = getSubtraction(50, 10);
-echo $result + 5;
-
-//echo $name . "\n";
-//echo $age . "\n";
-//echo $hobby . "\n";
-//echo $isMarried . "\n";
-//echo $pet . "\n";
-//echo $petName . "\n";
+$person = new Person();
+$person->sayHello();
